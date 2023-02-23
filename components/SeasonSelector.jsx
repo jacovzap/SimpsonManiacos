d6ff9271636c7938e43f117261c3b24be625e1da
jacovzap <jacovzap@gmail.com>
2023-02-23 17:15:03 -0400

    season selector component

--- components/SeasonSelector.jsx
+++ components/SeasonSelector.jsx
@@ -1,13 +1,46 @@
-import { BsCaretDownFill } from "react-icons/bs";
+'use client'
+
+import { useState } from 'react'
+import Link from 'next/link'
+
+import seasonsInfo from '@/assets/seasons.info'
+
+import { BsCaretDownFill } from 'react-icons/bs'
 
 const SeasonSelector = ({ season }) => {
+  const [isSelectOpen, setIsSelectOpen] = useState(false)
+  const invertedList = seasonsInfo.slice(0).reverse()
   return (
-    <div>
-      <button className='season-selector-button'>
+    <div className='season-selector'>
+      <button
+        className='season-selector-button'
+        onClick={() => {
+          setIsSelectOpen(!isSelectOpen)
+        }}
+      >
         Temporada {season}
-        <BsCaretDownFill/>
-        </button>
-      <div></div>
+        <BsCaretDownFill />
+      </button>
+      <div
+        className='season-selector-select'
+        style={
+          isSelectOpen ? { visibility: 'visible' } : { visibility: 'hidden' }
+        }
+      >
+        {invertedList.map(season => {
+          return (
+            <Link
+              key={`season-${season.id}`}
+              href={`/seasons/${season.id}`}
+              style={{ textDecoration: 'none' }}
+            >
+              <div className='season-selector-select-option'>
+                Temporada {season.id}
+              </div>
+            </Link>
+          )
+        })}
+      </div>
     </div>
   )
 }
